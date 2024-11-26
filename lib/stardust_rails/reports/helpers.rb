0a2a9392ad90df3825f2823ec438ea76a44209c4
Vic Amuso <vic@parablesoft.com>
2024-11-26 16:11:14 -0500

    Removal of default dates

--- lib/stardust_rails/reports/helpers.rb
+++ lib/stardust_rails/reports/helpers.rb
@@ -1,25 +1,24 @@
 module StardustRails
   module Reports
     module Helpers
-      def dates_from_variables(variables,variable_name,default_dates=nil)
-        lookup_value = variables[variable_name].to_s.underscore.gsub(/([\s.'_,])\1+/,"_")
+      def dates_from_variables(variables, variable_name, default_dates = nil)
+        lookup_value = variables[variable_name].to_s.underscore.gsub(/([\s.'_,])\1+/, "_")
 
         if lookup_value.downcase == "custom"
           dates = [
             Date.parse(variables["#{variable_name}_custom_start_at".to_sym]),
-            Date.parse(variables["#{variable_name}_custom_end_at".to_sym])
+            Date.parse(variables["#{variable_name}_custom_end_at".to_sym]),
           ]
         else
           dates = date_ranges.fetch(
             lookup_value,
-            default_dates||month_to_date_default
+            default_dates || [nil, nil]
           )
-
         end
 
         [
           dates.first.beginning_of_day,
-          dates.last.end_of_day
+          dates.last.end_of_day,
         ]
       end
 
@@ -27,11 +26,10 @@ module StardustRails
 
       EST_ZONE_NAME = "Eastern Time (US & Canada)"
 
-
       def month_to_date_default
         [
           now.beginning_of_month,
-          now.end_of_month
+          now.end_of_month,
         ]
       end
 
@@ -43,7 +41,6 @@ module StardustRails
         now - 1.year
       end
 
-
       def beginning_of_month
         now.beginning_of_month
       end
@@ -52,48 +49,48 @@ module StardustRails
         {
           "month_to_date" => [
             beginning_of_month,
-            now
+            now,
           ],
           "quarter_to_date" => [
             now.beginning_of_quarter,
-            now
+            now,
           ],
           "year_to_date" => [
             now.beginning_of_year,
-            now
+            now,
           ],
           "last_year_month_to_date" => [
             one_year_ago.beginning_of_month,
-            one_year_ago
+            one_year_ago,
           ],
           "last_year_quarter_to_date" => [
             one_year_ago.beginning_of_quarter,
-            one_year_ago
+            one_year_ago,
           ],
           "last_year_year_to_date" => [
             one_year_ago.beginning_of_year,
-            one_year_ago
+            one_year_ago,
           ],
           "this_week" => [
             now.beginning_of_week,
-            now
+            now,
           ],
           "last_week" => [
             now.last_week.beginning_of_week,
-            now.last_week.end_of_week
+            now.last_week.end_of_week,
           ],
           "last_month" => [
             now.last_month.beginning_of_month,
-            now.last_month.end_of_month
+            now.last_month.end_of_month,
           ],
           "last_quarter" => [
             now.last_quarter.beginning_of_quarter,
-            now.last_quarter.end_of_quarter
+            now.last_quarter.end_of_quarter,
           ],
           "last_year" => [
             now.last_year.beginning_of_year,
-            now.last_year.end_of_year
-          ]
+            now.last_year.end_of_year,
+          ],
         }
       end
     end
